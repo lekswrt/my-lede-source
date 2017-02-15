33d75d5ade42a81b88d5ee3270f52902991b6538
Thibaut VARENE <hacks@slashdirt.org>
2017-02-15 11:40:06 +0100

    ar71xx: Enable mtdsplit support for RB SPI NOR devices
    This patch enables mtdsplit for the MikroTik subtarget.
    It converts mach-rbspi.c to use a single "firmware" partition.
    Finally, it converts the SPI NOR profile to metadata images:
    the original combined-image format is gone, the images are now
    "standard" sysupgrade images with metadata appended.
    
    Note: kernel2minor apparently pads the kernel container to erase-block
    boundary, but this is undocumented behaviour, so we do not rely on it
    and call pad-to anyway.
    
    Note: in platform.sh, the boards are tested last in platform_check_image()
    as this should eventually become the "*)" default case when more devices
    switch to metadata images.
    
    Signed-off-by: Thibaut VARENE <hacks@slashdirt.org>
    
    Conflicts:
    	target/linux/ar71xx/image/mikrotik.mk

--- target/linux/ar71xx/image/mikrotik.mk
+++ target/linux/ar71xx/image/mikrotik.mk
@@ -31,10 +31,10 @@ define Device/rb-nor-flash-16M
   IMAGE_SIZE := 16000k
   LOADER_TYPE := elf
   KERNEL_INSTALL := 1
-  KERNEL := kernel-bin | lzma | loader-kernel | kernel2minor -s 1024 -e
-  KERNEL_INITRAMFS := kernel-bin | lzma | loader-kernel
-  IMAGES := sysupgrade.bin
-  IMAGE/sysupgrade.bin = append-rootfs | pad-rootfs | combined-image | check-size $$$$(IMAGE_SIZE)
+  KERNEL := kernel-bin | lzma | loader-kernel
+  SUPPORTED_DEVICES := rb-750-r2 rb-750up-r2 rb-941-2nd rb-951ui-2nd rb-mapl-2nd
+  IMAGE/sysupgrade.bin = append-kernel | kernel2minor -s 1024 -e | pad-to $$$$(BLOCKSIZE) | \
+	append-rootfs | pad-rootfs | append-metadata | check-size $$$$(IMAGE_SIZE)
 endef
 
 TARGET_DEVICES += rb-nor-flash-16M
